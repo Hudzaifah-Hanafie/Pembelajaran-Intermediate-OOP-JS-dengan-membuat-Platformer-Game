eac711479b3214c60689583dcab17605b8319642
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-09-04 16:25:40 +0700

    menambahkan class CheckPoint, objek checkpointPositions, objek class checkpoints, isi animate untuk menambah animasi pada checkpoint menggunakan forEach, fungsi showCheckpointScreen()

--- script.js
+++ script.js
@@ -68,6 +68,27 @@ class Platform {
     }
 }
 
+class CheckPoint {
+    constructor(x, y, z) {
+        this.position = {x, y};
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(70);
+        this.claimed = false;
+    }
+
+    draw() {
+        ctx.fillStyle = "#f1be32";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    claim() {
+        this.width = 0;
+        this.height = 0;
+        this.position.y = Infinity;
+        this.claimed = true;
+    }
+}
+
 const player = new Player();
 
 const platformPositions = [
@@ -87,10 +108,19 @@ const platformPositions = [
 
 const platforms = platformPositions.map((platform) => new Platform(platform.x, platform.y));
 
+const checkpointPositions = [
+    {x: 1170, y: proportionalSize(80), z: 1},
+    {x: 2900, y: proportionalSize(330), z: 2 },
+    {x: 4800, y: proportionalSize(80), z: 3}
+];
+
+const checkpoints = checkpointPositions.map((checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z));
+
 const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     platforms.forEach((platform) => platform.draw());
+    checkpoints.forEach((checkpoint) => checkpoint.draw());
     player.update();
     if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
         player.velocity.x = 5;
@@ -100,8 +130,10 @@ const animate = () => {
         player.velocity.x = 0;
         if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
             platforms.forEach((platform) => platform.position.x -= 5);
+            checkpoints.forEach((checkpoint) => checkpoint.position.x -= 5);
         } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
             platforms.forEach((platform) => platform.position.x += 5);
+            checkpoints.forEach((checkpoint) => checkpoint.position.x += 5);
         }
     }
 
@@ -127,6 +159,28 @@ const animate = () => {
         if (platformDetectionRules.every((rule) => rule)) {
             player.position.y = platform.position.y + player.height;
             player.velocity.y = gravity;
+        }
+    });
+
+    checkpoints.forEach((checkpoint, index, checkpoints) => {
+        const checkpointDetectionRules = [
+            player.position.x >= checkpoint.position.x,
+            player.position.y >= checkpoint.position.y,
+            player.position.y + player.height <= checkpoint.position.y + checkpoint.height,
+            isCheckpointCollisionDetectionActive,
+            player.position.x - player.width <= checkpoint.position.x - checkpoint.width + player.width * 0.9,
+            index === 0 || checkpoints[index - 1].claimed === true
+        ];
+
+        if (checkpointDetectionRules.every((rule) => rule)) {
+            checkpoint.claim();
+            if (index === checkpoints.length - 1) {
+                isCheckpointCollisionDetectionActive = false;
+                showCheckpointScreen("You reached the final checkpoint!");
+                movePlayer("ArrowRight", 0, false);
+            } else if (player.position.x >= checkpoint.position.x && player.position.x <= checkpoint.position.x + 40) {
+                showCheckpointScreen("You reached a checkpoint!");
+            }
         }
     });
 }
@@ -170,6 +224,16 @@ const startGame = () => {
     canvas.style.display = "block";
     startScreen.style.display = "none";
     animate();
+}
+
+const showCheckpointScreen = (msg) => {
+    checkpointScreen.style.display = "block";
+    checkpointMessage.textContent = msg;
+    if (isCheckpointCollisionDetectionActive) {
+        setTimeout(() => {
+            checkpointScreen.style.display = "none";
+        }, 2000);
+    }
 }
 
 startBtn.addEventListener('click', startGame);
